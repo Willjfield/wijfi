c50c916909d0cb071599b7398bb361f434390b11
Willjfield <willjfield@gmail.com>
2023-10-27 12:04:40 -0400

    1027

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,53 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <HelloWorld msg="Will J. Field" />
+  <v-app>
+    <v-app-bar app color="primary">
+      <v-app-bar-title>
+        <h2>Will J Field</h2>
+      </v-app-bar-title>
+      <template v-slot:append>
+        <v-btn href="https://github.com/willjfield" target="_blank">
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+        <v-btn href="https://fosstodon.org/@Wijfi" target="_blank">
+
+          <v-icon>mdi-mastodon</v-icon>
+        </v-btn>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <v-container class="bg-surface-variant">
+        <v-row class="mb-6" no-gutters>
+          <v-col>
+              <v-card class="pa-2 ma-2">
+                lorem ipsum
+              </v-card>
+          </v-col>
+          <v-col>
+              <v-card class="pa-2 ma-2">
+                lorem ipsum
+              </v-card>
+          </v-col>
+          <v-col>
+              <v-card class="pa-2 ma-2">
+                lorem ipsum
+              </v-card>
+          </v-col>
+          <v-col>
+              <v-card class="pa-2 ma-2">
+                lorem ipsum
+              </v-card>
+          </v-col>
+          <v-col>
+              <v-card class="pa-2 ma-2">
+                lorem ipsum
+              </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
@@ -13,9 +57,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
