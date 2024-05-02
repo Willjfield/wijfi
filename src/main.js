d3481c8f183c99e881439618f9032a6affb0d199
Willjfield <willjfield@gmail.com>
2024-05-02 12:52:21 -0400

    soma pest started

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import mitt from 'mitt';
@@ -7,6 +8,23 @@ const emitter = mitt();
 
 const app = createApp(App)
 app.provide('mitt', emitter)
-app.use(vuetify)
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('./views/Home.vue')
+        },
+        {
+            path: '/SOMAPest',
+            name: 'somapest',
+            component: () => import('./views/SOMAPest.vue')
+        }
+    ]
+})
+
+app.use(vuetify).use(router)
 
 app.mount('#app')
---
+++ src/views/SOMAPest.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <h1>SOMA Pesticide Map</h1>
+        <v-container>
+            <v-sheet class="text-left">
+            <v-card justify="start" subtitle="The goal of the project" text="" title="Overview"></v-card>
+            <v-card subtitle="What the final output of the map will be" text="" title="Output"></v-card>
+            <v-card subtitle="How to contribute" title="Contributing">
+                <v-card-text>
+                <a href="https://app.qfield.cloud/accounts/signup/">https://app.qfield.cloud/accounts/signup/</a>
+                </v-card-text>
+            </v-card>
+            </v-sheet>
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+import ml from 'maplibre-gl';
+export default {
+    name: 'SOMAPest',
+    mounted() {
+
+    }
+}
+</script>
