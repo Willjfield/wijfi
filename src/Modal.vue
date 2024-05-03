8d9d24d983c6f4a599d6f1fd4b9e0460cdce4970
Will Field <wfield@gc.cuny.edu>
2024-05-03 16:31:48 -0400

    mostly ready

--- src/Modal.vue
+++ src/Modal.vue
@@ -1,69 +1,67 @@
 <template>
-<v-dialog
-      v-model="modal"
-      fullscreen
-    >
-      <v-card>
-        <v-card-actions>
-          <v-btn class="close-modal-x" color="primary" @click="modal = false">X</v-btn>
-        </v-card-actions>
-        <v-card-title>  <a class="external-link" :href="this.project.link">{{ project.title }}</a></v-card-title>
-        <v-card-text>
-           {{ project.summary}}
-           <br>
-           <v-divider></v-divider>
-           <br>
-           <div class="img-container">
-           <img class="project-image" :src="project.thumbnails[0]">
-          </div>
-           <v-divider></v-divider>
-           <br>
-          <a class="external-link" :href="this.project.link">Link</a> |
-          Employer/Client: {{ project.employer }} |
-          Role: {{  project.role }}
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" block @click="modal = false">Close</v-btn>
-        </v-card-actions>
-      </v-card>
-</v-dialog>
+  <v-dialog v-model="modal" fullscreen>
+    <v-card>
+      <v-card-actions>
+        <v-btn class="close-modal-x" color="primary" @click="modal = false">X</v-btn>
+      </v-card-actions>
+      <v-card-title> <a class="external-link" :href="this.project.link">{{ project.title }}</a></v-card-title>
+      <v-card-text>
+        {{ project.summary }}
+        <br>
+        <v-divider></v-divider>
+        <br>
+        <div class="img-container">
+          <img class="project-image" :src="project.thumbnails[0]">
+        </div>
+        <v-divider></v-divider>
+        <br>
+        <a class="external-link" :href="this.project.link">Link</a> |
+        Employer/Client: {{ project.employer }} |
+        Role: {{ project.role }}
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" block @click="modal = false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 <script>
-import {inject} from 'vue';
+import { inject } from 'vue';
 export default {
-     name: 'Modal',
-    data() {
-        return {
-            modal: false,
-            project: {}
-        }
-    },
-    mounted() {
-        
+  name: 'Modal',
+  data() {
+    return {
+      modal: false,
+      project: {}
+    }
+  },
+  mounted() {
+
     const mitt = inject('mitt');
     mitt.on('open-modal', (project) => {
-        this.project = project;
+      this.project = project;
       this.modal = true;
-    })
-  
+    });
 
-    }
+  }
 }
 </script>
 <style scoped>
 .close-modal-x {
-    position: absolute;
-    right: 0;
-    font-size: x-large;
-    font-family: sans-serif;
+  position: absolute;
+  right: 0;
+  font-size: x-large;
+  font-family: sans-serif;
 }
-.img-container{
+
+.img-container {
   display: block;
   margin-left: auto;
   margin-right: auto;
   width: 50%;
 }
-.project-image{
+
+.project-image {
   max-width: 100%;
   max-height: 50%;
   margin: 0 auto;
--- src/views/SOMAPest.vue
+++ src/views/SOMAPest.vue
@@ -14,7 +14,7 @@
 
             <v-divider></v-divider>
             <v-sheet class="text-left ma-4 pa-4">
-                <v-card justify="start" title="The Goal">
+                <v-card class="ma-4 pa-4" justify="start" title="The Goal">
                     <v-card-text>
                         The goal of the project is to collect and make publicly available data about where residential
                         and commercial pesticides are being used in South Orange and Maplewood. The data may be used for
@@ -26,7 +26,57 @@
                             href="https://www.pollinator-pathway.org/pesticides/">https://www.pollinator-pathway.org/pesticides/</a>
                     </v-card-text>
                 </v-card>
-                <v-card title="Output">
+
+                <v-card class="ma-4 pa-4" subtitle="How to contribute to the project" title="Contributing">
+                    <v-card-text>
+                        This section describes how to download and set up the project so that you can contribute data
+                        (Steps 1-2) and how to update and share the data (Steps 3-5). The project uses <a
+                            href="https://www.qfield.org/">QField</a> and <a href="https://qfield.cloud">QField
+                            Cloud</a>. QField is an application for collecting geographic based data and QField Cloud is
+                        a free
+                        online service that syncs data in the QField app.
+                    </v-card-text>
+                    <v-card class=" ma-4 pa-4" title="1) Sign up for a free qfield cloud account">
+                        <v-card-text>
+                            The first step in contributing is to sign up for a free qfield cloud account from
+                            <a
+                                href="https://app.qfield.cloud/accounts/signup/">https://app.qfield.cloud/accounts/signup/</a>
+                            <br>Once you have signed up, email <a href="mailto:wijfi@proton.me">wijfi@proton.me</a> with
+                            the Username or Email that you
+                            signed up
+                            with and I will add
+                            you as a collaborator
+
+                        </v-card-text>
+                    </v-card>
+                    <v-card class="ma-4 pa-4" title="2) Download the qfield app">
+                        <v-card-text>
+                            <a href="https://www.qfield.org">https://www.qfield.org</a> has links to download QField
+                            from Google Play and the App Store or you can search from inside the store. Download and
+                            install QField on your device.
+                        </v-card-text>
+                    </v-card>
+                    <v-card class="ma-4 pa-4" title="3) Connect to and syncronize the project">
+                        <v-card-text>
+                            <a
+                                href="https://app.qfield.cloud/accounts/signup/">https://app.qfield.cloud/accounts/signup/</a>
+                        </v-card-text>
+                    </v-card>
+                    <v-card class="ma-4 pa-4" title="4) Update a parcel">
+                        <v-card-text>
+                            <a
+                                href="https://app.qfield.cloud/accounts/signup/">https://app.qfield.cloud/accounts/signup/</a>
+                        </v-card-text>
+                    </v-card>
+                    <v-card class="ma-4 pa-4" title="5) Push updates">
+                        <v-card-text>
+                            <a
+                                href="https://app.qfield.cloud/accounts/signup/">https://app.qfield.cloud/accounts/signup/</a>
+                        </v-card-text>
+                    </v-card>
+
+                </v-card>
+                <v-card class="ma-4 pa-4" title="Output">
                     <v-card-text>
                         The final output of the project will be an interactive web map to help non-technical users to
                         explore the data, as well as the underlying data in the form of GIS files (geojson and
@@ -41,20 +91,35 @@
                                 the course of the project or that the parcel has not been marked as using pesticides.
                             </v-list-item>
                             <v-list-item title="PEST_USE">
-                                {{ JSON.stringify(useMapping, null, 4) }}
+                                This field contains the most recent observation of pesticide use on the parcel by noting
+                                the name of the company that put up the flag or lawn sign. Additional values for this
+                                can be added. Null values are assumed to mean that no pesticide use has been identified
+                                on the parcel.
+                                <v-table>
+                                    <thead>
+                                        <tr>
+                                            <th class="text-left">
+                                                Code
+                                            </th>
+                                            <th class="text-left">
+                                                Value
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="item in Object.keys(useMapping)" :key="item">
+                                            <td>{{ item }}</td>
+                                            <td>{{ useMapping[item] }}</td>
+                                        </tr>
+                                    </tbody>
+                                </v-table>
                             </v-list-item>
-                            <v-list-item title="Parcel Geometry"><a
-                                    href="https://nj.gov/njgin/edata/parcels/">https://nj.gov/njgin/edata/parcels/</a>
+                            <v-list-item title="Parcel Geometry">Parcel geometry is provided by NJ.gov<br>
+                                <a href="https://nj.gov/njgin/edata/parcels/">https://nj.gov/njgin/edata/parcels/</a>
                                 <br>
                                 <a
                                     href="https://www.arcgis.com/home/item.html?id=34f9940598fc409094a28032e54c864e">https://www.arcgis.com/home/item.html?id=34f9940598fc409094a28032e54c864e</a></v-list-item>
                         </v-list>
-                    </v-card-text>
-                </v-card>
-                <v-card subtitle="How to contribute" title="Contributing">
-                    <v-card-text>
-                        <a
-                            href="https://app.qfield.cloud/accounts/signup/">https://app.qfield.cloud/accounts/signup/</a>
                     </v-card-text>
                 </v-card>
             </v-sheet>
@@ -81,7 +146,7 @@ export default {
     data() {
         return {
             useMapping: {
-                
+
                 'Lc': 'Lawn Company',
                 'MsqSh': 'Mosquito Shield',
                 'MsqSq': 'Mosquito Squad',
@@ -90,7 +155,7 @@ export default {
                 'Other': 'Other',
                 'Grn': 'Green Roots',
                 'Dia': 'Dial Environmental'
-            
+
             }
         }
     },
@@ -98,7 +163,7 @@ export default {
 
         const map = new ml.Map({
             container: 'map', // container id
-            style: 'basic.json', // style URL
+            style: '/basic.json', // style URL
             center: [-74.2644, 40.73766], // starting position [lng, lat]
             zoom: 12.65 // starting zoom
         });
@@ -114,7 +179,7 @@ export default {
             const coordinates = map.unproject(e.point);
             const use = e.features[0].properties.PEST_USE;
             const useMapping = this.useMapping;
-            popup.setLngLat(coordinates).setHTML(use ? useMapping[use] || `Other (${use})`  : 'none or no data').addTo(map);
+            popup.setLngLat(coordinates).setHTML(use ? useMapping[use] || `Other (${use})` : 'none or no data').addTo(map);
 
         });
 
